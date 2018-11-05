19985531f58daec987b99325f35b5feb14c52c91
bkorinnyi <bohdan.korinnyi@gmail.com>
2018-11-06 00:09:09 +0200

    add aws clients

--- src/main/java/com/example/demo/TextRecognitionApplication.java
+++ src/main/java/com/example/demo/TextRecognitionApplication.java
@@ -1,12 +1,42 @@
 package com.example.demo;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static com.amazonaws.regions.Regions.US_WEST_2;
 
 @SpringBootApplication
 public class TextRecognitionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TextRecognitionApplication.class, args);
+    }
+
+    @Bean
+    public AmazonRekognition amazonRekognition() {
+        return AmazonRekognitionClientBuilder.standard()
+                .withCredentials(awsCredentialsProvider())
+                .withRegion(US_WEST_2)
+                .build();
+    }
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(awsCredentialsProvider())
+                .withRegion(US_WEST_2)
+                .build();
+    }
+
+    @Bean
+    public AWSCredentialsProvider awsCredentialsProvider() {
+        return new EnvironmentVariableCredentialsProvider();
     }
 }
