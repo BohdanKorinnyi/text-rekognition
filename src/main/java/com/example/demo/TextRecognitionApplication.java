8f7cfcbfe2afda42b74a5e1f0467dfbf77741c5a
bkorinnyi <bohdan.korinnyi@gmail.com>
2018-11-05 23:36:41 +0200

    init project

---
+++ src/main/java/com/example/demo/TextRecognitionApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextRecognitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextRecognitionApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/demo/TextRecognitionApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TextRecognitionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
